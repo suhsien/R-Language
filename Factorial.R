c99e4404075e8a6173d1cda2d7c1131c9612cacc
suhsien <suhsienhuang@gmail.com>
2016-08-17 12:50:06 +0800

    Revert "Factorial"
    This reverts commit 7467b5a8b3748aeec68ee9127a300caee8e5b516.

--- Factorial.R
+++
@@ -1,7 +0,0 @@
-﻿# 5!=5*4*3*2*1=120
-#在R中如何使用Function計算階乘
-myfun <- function(x=0){
-  if (x==0) return (1) else{ y <- x * myfun(x-1); return (y)}
-}
-
-myfun(5)
--- plot.R
+++
@@ -1,6 +0,0 @@
-x <- c(1:100)
-y <- rnorm(100)*100
-hist(y)
-test.model <- lm(y~x)
-test.model
-plot(x,y)
