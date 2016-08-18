14deeaf70ab65a196a8a76773c999bfaefaa8b07
suhsien <suhsienhuang@gmail.com>
2016-08-18 14:45:40 +0800

    Clustering

---
+++ kmeans.R
@@ -0,0 +1,16 @@
+
+data(iris)
+attributes(iris)
+
+iris2 <- iris
+iris2$Species <- NULL
+
+(kmeans.result <- kmeans(iris2, 3))
+
+table(iris$Species, kmeans.result$cluster)
+
+
+#  Plot cluster centers
+plot(iris2[c("Sepal.Length", "Sepal.Width")], col = kmeans.result$cluster)
+points(kmeans.result$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex=2)
+
